395d1edd437eab3e3065088616ddc14e87da231f
RedCrafter07 <github@redcrafter07.de>
2022-08-15 21:47:35 +0200

    Remove the divider

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,25 +1,97 @@
-import { Divider } from '@mantine/core';
+import { Button, Dialog, Divider, Group, Text, TextInput } from '@mantine/core';
+import { useEffect, useRef, useState } from 'react';
 import Base from '../components/Base';
 import Link from '../components/Link';
+import trpc, { Msg } from '../util/trpc';
+import { useLocalStorage } from '@mantine/hooks';
+import { showNotification } from '@mantine/notifications';
 
 export default function Home() {
+	// fetch current messages
+	const messages = trpc.useQuery(['messages']);
+	// set messages state for rendering & further manipulation
+	const [msgs, setMsgs] = useState<typeof messages['data']>();
+	const [name, setName] = useLocalStorage({
+		key: 'name',
+	});
+	const [canChat, setCanChat] = useState(
+		name != null && name != undefined && name.length > 0,
+	);
+
+	useEffect(() => {
+		setMsgs(messages.data || []);
+	}, [messages.data]);
+
+	// subscribe to message changes
+	trpc.useSubscription(['onAdd'], {
+		onNext(msg) {
+			//merge current messages with new message
+			setMsgs((msgs) => [...(msgs || []), msg]);
+		},
+	});
+
+	const loadedMsgs = msgs?.slice(-7) || [];
+
+	// add a mutation to add a message
+	const addMsg = trpc.useMutation(['messages.add']);
+
 	return (
 		<Base>
-			<>
-				<h1>Retmod Template</h1>
-				<h3 className='opacity-50'>with Daisyui, Mantine and SCSS</h3>
-				<Divider className='my-4' />
-				<p>This is a Retmod Project!</p>
-				<br />
-				<p className='btn btn-wide btn-info btn-outline'>
-					This is a DaisyUI button!
-				</p>
-				<br />
-				<br />
-				<Link href='/test' className='btn btn-wide btn-outline btn-accent'>
-					This is a page transition button
-				</Link>
-			</>
+			<div className='overflow-hidden'>
+				<div className='flex flex-col max-h-screen'>
+					<Dialog opened={!canChat} size='lg' radius='md'>
+						<Text size='sm' style={{ marginBottom: 10 }} weight={500}>
+							Enter your username
+						</Text>
+
+						<Group align='flex-end'>
+							<TextInput
+								placeholder='RedCrafter07'
+								style={{ flex: 1 }}
+								onKeyDown={(e) => {
+									if (e.key === 'Enter') {
+										if (e.currentTarget.value.trim().length > 0) {
+											setName(e.currentTarget.value);
+											setCanChat(true);
+										}
+									}
+								}}
+							/>
+						</Group>
+					</Dialog>
+					<div className='h-[90vh] overflow-y-auto flex flex-col justify-end'>
+						{loadedMsgs?.map((m) => (
+							<div className='bg-base-300 my-2 p-4 rounded-md'>
+								<h5>{m.user}</h5>
+								<h3>{m.message}</h3>
+							</div>
+						))}
+					</div>
+					<div className='w-full py-4 grid place-items-center'>
+						{canChat ? (
+							<input
+								type='text'
+								placeholder='Message'
+								className='p-2 bg-base-300 w-full outline-none rounded-md'
+								onKeyDown={async (e) => {
+									if (!(e.currentTarget.value.trim().length > 0)) return;
+									if (e.code === 'Enter') {
+										addMsg.mutate({
+											message: e.currentTarget.value,
+											user: name as string,
+										});
+										e.currentTarget.value = '';
+									}
+								}}
+							/>
+						) : (
+							<Button fullWidth variant='outline' disabled>
+								Enter name!
+							</Button>
+						)}
+					</div>
+				</div>
+			</div>
 		</Base>
 	);
 }
