c09a32b7396cc0d7759cb254e27cd904b6fdfae4
RedCrafter07 <github@redcrafter07.de>
2022-08-15 21:52:27 +0200

    Update wrappers
    - tRPC Provider for further tRPC usage
    - QueryClientProvider as needed by tRPC
    - define the Websocket client, tRPC client & query client

--- client/src/wrappers.tsx
+++ client/src/wrappers.tsx
@@ -1,13 +1,35 @@
 import Router from './router';
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { ModalsProvider } from '@mantine/modals';
+import trpc from './util/trpc';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { createWSClient, wsLink } from '@trpc/client/links/wsLink';
+
+const wsClient = createWSClient({
+	url: `ws://${window.location.host}`,
+});
+
+const queryClient = new QueryClient();
+const trpcClient = trpc.createClient({
+	url: `${window.location.origin}/trpc`,
+	links: [
+		wsLink({
+			client: wsClient,
+		}),
+	],
+});
 
 export default function Wrappers() {
 	return (
 		<div className='text-base-content bg-base-300'>
 			<MantineProvider theme={{ primaryColor: 'cyan', colorScheme: 'dark' }}>
 				<NotificationsProvider>
-					<Router />
+					<trpc.Provider queryClient={queryClient} client={trpcClient}>
+						<QueryClientProvider client={queryClient}>
+							<Router />
+						</QueryClientProvider>
+					</trpc.Provider>
 				</NotificationsProvider>
 			</MantineProvider>
 		</div>
