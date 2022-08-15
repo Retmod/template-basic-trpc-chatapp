115168ff12bab66096a1f191b3f919b5832018b0
RedCrafter07 <github@redcrafter07.de>
2022-08-15 21:46:57 +0200

    Change base component to accept multiple children

--- client/src/components/Base.tsx
+++ client/src/components/Base.tsx
@@ -1,4 +1,4 @@
-export default function Base(props: { children: JSX.Element }) {
+export default function Base(props: { children: React.ReactNode }) {
 	return (
 		<div className='bg-base-100 text-base-content min-h-screen p-2'>
 			<div className='mx-auto container'>{props.children}</div>
